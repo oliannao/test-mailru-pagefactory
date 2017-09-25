453a127fe686cc20df66043cad793ba47f29d0c3
oliannao <31596803+oliannao@users.noreply.github.com>
2017-09-25 16:40:13 +0300

    Create src\test\java\pages\Page.java

---
+++ src\test\java\pages\Page.java
@@ -0,0 +1,18 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public abstract class Page
+{
+    protected  final WebDriver driver;
+
+    public Page(WebDriver driver)
+    {
+        this.driver = driver;
+    }
+
+    public WebDriver getDriver()
+    {
+        return this.driver;
+    }
+}
